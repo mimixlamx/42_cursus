c942b3cadcbc8cce4ff87d0898af6df596f1402a
mimixlamx <maxime.bruyere74@gmail.com>
2025-10-02 12:22:22 +0200

    add tolower

---
+++ ft_tolower.c
@@ -0,0 +1,27 @@
+/*
+#include <unistd.h>
+#include <stdio.h>
+*/
+int	tolower(int c);
+
+int tolowerr(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (c + 32);
+	else
+		return (c);
+}
+/*
+int	main(void)
+{
+	int	tkt;
+
+	tkt = 'Z';
+	write (1, &tkt, 1);
+	printf ("tkt before = %d\n", tkt);
+	tkt = tolowerr(tkt);
+	write (1, &tkt,1);
+	printf ("tkt after = %d\n", tkt);
+	return (0);
+}
+*/
