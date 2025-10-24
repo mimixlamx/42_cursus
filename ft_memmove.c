da20fc90e8cdc4e485bec90252937d33c73cf6b7
Maxime Bruyere <mbruyere@c1r9s6.42lausanne.ch>
2025-10-24 14:09:58 +0200

    ok francinette

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mbruyere <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 10:00:48 by mbruyere          #+#    #+#             */
-/*   Updated: 2025/10/17 18:32:09 by mbruyere         ###   ####lausanne.ch   */
+/*   Updated: 2025/10/24 14:09:13 by mbruyere         ###   ####lausanne.ch   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <stdio.h>
 */
 #include <stddef.h>
+#include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	reversemove(unsigned char *d, const unsigned char *s, size_t n);
@@ -23,20 +24,27 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char		*d;
 	const unsigned char	*s;
+	size_t				i;
 
+	i = 0;
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
-	if (dest == NULL && src == NULL)
-		return (NULL);
-	if (n == 0 || dest == src)
+	if (d == s || n == 0)
 		return (dest);
 	if (d < s)
-		move(d, s, n);
+		ft_memcpy(dest, src, n);
 	else
-		reversemove(d, s, n);
+	{
+		i = n;
+		while (i > 0)
+		{
+			i--;
+			d[i] = s[i];
+		}
+	}
 	return (dest);
 }
-
+/*
 void	move(unsigned char *d, const unsigned char *s, size_t n)
 {
 	size_t	i;
@@ -47,20 +55,17 @@ void	move(unsigned char *d, const unsigned char *s, size_t n)
 		d[i] = s[i];
 		i++;
 	}
-
 }
 
 void	reversemove(unsigned char *d, const unsigned char *s, size_t n)
 {
-	int i;
-
-	i = n;
-	while (i > 0 )
+	while (n > 0)
 	{
-		i--;
-		d[i] = s[i];
+		n--;
+		d[n] = s[n];
 	}
 }
+*/
 /*
 int	main(void)
 {
