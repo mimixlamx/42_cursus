b273d38c57b321d8b62482e5d923076cb7e77ca0
mimixlamx <maxime.bruyere74@gmail.com>
2025-11-07 10:08:26 +0100

    tkt

--- 42_printf/ft_putvoidptr.c
+++ 42_printf/ft_putvoidptr.c
@@ -4,6 +4,13 @@
 int	ft_putvoidptr(void *ptr)
 {
 	write(1,"x0", 2);
-	ft_putnbr_base(, "0123456789ABCDEF", 0);
+	ft_putnbr_base(&(int)*ptr, "0123456789ABCDEF", 0);
 	return (2);
+}
+
+int	main(void)
+{
+	void *test;
+
+	ft_putvoidptr((void *)test);
 }
