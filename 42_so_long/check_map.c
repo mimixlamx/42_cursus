212172e60cce02975b22f7dfded93bb42e588f11
Maxime Bruyere <mbruyere@c2r1s4.42lausanne.ch>
2026-02-10 16:55:30 +0100

    big work on reducing the code using a struct

--- 42_so_long/check_map.c
+++ 42_so_long/check_map.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                         ::::::::           */
+/*   check_map.c                                         :+:    :+:           */
+/*                                                      +:+                   */
+/*   By: mbruyere <marvin@42.fr>                       +#+                    */
+/*                                                    +#+                     */
+/*   Created: 2026/02/10 14:27:41 by mbruyere       #+#    #+#                */
+/*   Updated: 2026/02/10 16:45:14 by mbruyere       ########   odam.nl        */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*
  ** file dedicated to check the map on so long project 
 */
@@ -10,83 +22,86 @@
 #include <fcntl.h>
 #include <stdio.h> // to be deleted
 
-int	flood_fill(int w, int h, int x, int y, int *c, int *e, char **map)
+typedef struct	s_check
+{
+	char	**map;
+	char	**visited_map;
+	int		w;
+	int		h;
+	int		x;
+	int		y;
+	int		p;
+	int		e;
+	int		c;
+	int		found_c;
+	int		found_e;
+}	t_check;
+
+int	flood_fill(int x, int y, t_check *check)
 {
 	printf ("inside floodfill\n");
-	if (x < 0 || y < 0 || x > h || y > w)
+	if (x < 0 || y < 0 || x > check->h || y > check->w)
 		return (printf("out of mapp\n"), 0);
-	if (map[x][y] == '1' || map[x][y] == 'V')
-		return(printf("reach wall or visited\n"), 0);
-	if (map[x][y] == 'C')
-		(*c)++;
-	if (map[x][y] == 'E')
-		(*e)++;
-	map[x][y] = 'V';
-	printf ("count this bitch %s \n", map[x]);
-	flood_fill(w, h, x + 1, y, c, e, map);
-	flood_fill(w, h, x - 1, y, c, e, map);
-	flood_fill(w, h, x, y + 1, c, e, map);
-	flood_fill(w, h, x, y - 1, c, e, map);
+	if (check->visited_map[x][y] == '1' || check->visited_map[x][y] == 'V')
+		return (printf("reach wall or visited\n"), 0);
+	if (check->visited_map[x][y] == 'C')
+		check->found_c++;
+	if (check->visited_map[x][y] == 'E')
+		check->found_e++;
+	check->visited_map[x][y] = 'V';
+	printf ("count this bitch %s \n", check->visited_map[x]);
+	flood_fill(x + 1, y, check);
+	flood_fill(x - 1, y, check);
+	flood_fill(x, y + 1, check);
+	flood_fill(x, y - 1, check);
 	return (printf("floodfill end\n"), 1);
 }
 
-int	flood_fil_launch(int w, int h, int x, int y, int c, char **map)
+int	flood_fil_launch(t_check *check)
 {
-	char	**visited_map;
-	int		found_c;
-	int		found_e;
 	int		i;
 
 	i = 0;
-	found_c = 0;
-	found_e = 0;
-	visited_map = ft_calloc(h + 1, sizeof (char*));
-	while (i < h)
+	check->found_c = 0;
+	check->found_e = 0;
+	check->visited_map = ft_calloc(check->h + 1, sizeof(char *));
+	while (i < check->h)
 	{
-		visited_map[i] = ft_strdup(map[i]);
-		printf("visited_map[%d] = %s w = %d\n", i, visited_map[i], w);
+		check->visited_map[i] = ft_strdup(check->map[i]);
+		printf("visited_map[%d] = %s w = %d\n", i, check->visited_map[i], check->w);
 		i++;
 	}
-	printf("launch floodfill %d %d %d %d\n",w ,h ,x ,y);
-	flood_fill(w, h, x, y, &found_c, &found_e, visited_map);
+	printf("launch floodfill %d %d %d %d\n", check->w, check->h, check->x, check->y);
+	flood_fill(check->x, check->y, check);
 	i = 0;
-	while (i < h)
+	while (i < check->h)
 	{
-		printf("visited_map[%d] = %s w = %d\n", i, visited_map[i], w);
+		printf("visited_map[%d] = %s w = %d\n", i, check->visited_map[i], check->w);
 		i++;
 	}
-
-	if (found_c != c || found_e != 1)
-		return(printf("error in colictibles or exit e = %d c = %d\n", found_e, found_c), 0);
+	if (check->found_c != check->c || check->found_e != 1)
+		return (printf("error in colictibles or exit e = %d c = %d\n", check->found_e, check->found_c), 0);
 	return (printf("no error in floodfill\n"), 1);
 }
 
-int	check_in_map(int size, char **map)
+int	check_in_map(t_check *check)
 {
 	int	i;
-	int	len_0;
-
 	int	y;
-	int	p;
-	int	c;
-	int	e;
-	int	px;
-	int	py;
 
-	p = 0;
-	c = 0;
-	e = 0;
+	check->p = 0;
+	check->c = 0;
+	check->e = 0;
 	y = 0;
 	i = 0;
-	len_0 = ft_strlen(map[0]);
-	while (i < size)
+	while (i < check->h)
 	{
-		if (i == 0 || i == size - 1)
+		if (i == 0 || i == check->h - 1)
 		{
-		printf ("first or last line i = %d\n", i);
-			while (y < len_0)
+			printf ("first or last line i = %d\n", i);
+			while (y < check->w)
 			{
-				if (map[i][y] == '1')
+				if (check->map[i][y] == '1')
 					y++;
 				else
 					return (printf("error in line %d not only 1\n", i), 0);
@@ -96,90 +111,85 @@ int	check_in_map(int size, char **map)
 		else
 		{
 			printf ("inside lines i =%d y = %d\n", i, y);
-			if (map[i][0] == '1' && map[i][len_0 - 1] == '1')
+			if (check->map[i][0] == '1' && check->map[i][check->w - 1] == '1')
 				y++;
 			else
-				return (printf("error border line map[%d]\n", i));
-			while (y > 0 && y <= len_0 - 2)
+				return (printf("error border line map[%d]\n", i), 0);
+			while (y > 0 && y <= check->w - 2)
 			{
-				if (map[i][y] == '0' || map[i][y] == 'P' || map[i][y] == 'E' || map[i][y] == 'C' || map[i][y] == '1')
+				if (check->map[i][y] == '0' || check->map[i][y] == 'P' || check->map[i][y] == 'E' || check->map[i][y] == 'C' || check->map[i][y] == '1')
 				{
-					if (map[i][y] == 'P')
+					if (check->map[i][y] == 'P')
 					{
-						px = i;
-						py = y;
-						p++;
+						check->x = i;
+						check->y = y;
+						check->p++;
 					}
-					else if (map[i][y] == 'E')
-						e++;
-					else if (map[i][y] == 'C')
-						c++;
+					else if (check->map[i][y] == 'E')
+						check->e++;
+					else if (check->map[i][y] == 'C')
+						check->c++;
 					y++;
 				}
 				else
-					return (printf("error char not ok in map %c in position [%d][%d] char %c\n", map[i][y], i, y, map[i][y]), 0);
+					return (printf("error char not ok in map %c in position [%d][%d] char %c\n", check->map[i][y], i, y, check->map[i][y]), 0);
 			}
 			y = 0;
 		}
 		i++;
 	}
-	if (p > 1 || p == 0)
-		return (printf("1 player needed, now %d\n", p), 0);
-	if (e > 1 || e == 0)
-		return (printf("1 exit needed, now %d\n", e), 0);
-	if (c == 0) 
-		return (printf("1 collectible  needed, now %d\n", c), 0);
-	if (flood_fil_launch(len_0, size, px, py, c, map) == 0)
-		return (printf("fuck floodfill\n"), 0);
-	else
-		return (printf("no error in mapp"), 1);
+	if (check->p > 1 || check->p == 0)
+		return (printf("1 player needed, now %d\n", check->p), 0);
+	if (check->e > 1 || check->e == 0)
+		return (printf("1 exit needed, now %d\n", check->e), 0);
+	if (check->c == 0)
+		return (printf("1 collectible  needed, now %d\n", check->c), 0);
+	return (printf("no error in mapp\n"), 1);
 }
 
-int	check_size_map(int size, char **map)
+int	check_size_map(t_check	*check)
 {
 	int	i;
-	int	len_0;
 
 	i = 1;
-	len_0 = ft_strlen(map[0]);
-	while (map[i] && len_0 == ft_strlen(map[i]))
+	check->w = ft_strlen(check->map[0]);
+	while (check->map[i] && check->w == ft_strlen(check->map[i]))
 	{
-		printf ("len_0 = %d\n", len_0);
-		printf ("len_%d= %ld\n", i, ft_strlen(map[i]));
+		printf ("len_0 = %d\n", check->w);
+		printf ("len_%d= %ld\n", i, ft_strlen(check->map[i]));
 		i++;
 	}
-	if (i != size)
-		return (printf("error in size of a line\nlen_0 = %d vs len_%d = %zu\n",len_0, i,ft_strlen(map[i])), 0);
-	if (len_0 == size)
-		return (printf("error square map\n"),0);
+	if (i != check->h)
+		return (printf("error in size of a line\nlen_0 = %d vs len_%d = %zu\n", check->w, i, ft_strlen(check->map[i])), 0);
+	if (check->w == check->h)
+		return (printf("error square map\n"), 0);
 	else
-		return (printf("'no error on size mapp\n"),1);
+		return (printf("'no error on size mapp\n"), 1);
 }
 
 int	main(int argc, char **argv)
 {
-	char	**map;
 	char	*temp;
-	int		size;
 	int		i;
 	int		fd;
 	int		len_temp;
+	t_check	check;
 
+	check.h = 0;
 	i = 0;
-	size = 0;
 	if (argc != 2)
-		return (printf("pls enter only the mapp as an argument\n"), 0);// attention remplacer par ftprintf
-	if (ft_strncmp((argv[1] + (ft_strlen(argv[1]) - 4)),".ber", 4) != 0)
-		return (printf ("pls enter only a .ber map\n"), 0);// idem
+		return (printf("pls enter only the mapp as an argument\n"), 0);
+	if (ft_strncmp((argv[1] + (ft_strlen(argv[1]) - 4)), ".ber", 4) != 0)
+		return (printf ("pls enter only a .ber map\n"), 0);
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 		return (printf("pas d'ouverture fichier inexistant?\n"), 1);
 	while ((temp = get_next_line(fd)) != NULL)
-		size++;
-	printf("size = %d\n", size);
-	if (size == 0)
-		return(printf("empty map\n"), 0);
-	map = ft_calloc(size + 1, sizeof (char*));
+		check.h++;
+	printf("h = %d\n", check.h);
+	if (check.h == 0)
+		return (printf("empty map\n"), 0);
+	check.map = ft_calloc(check.h + 1, sizeof (char *));
 	close(fd);
 	fd = open(argv[1], O_RDONLY);
 	while ((temp = get_next_line(fd)) != NULL)
@@ -188,18 +198,21 @@ int	main(int argc, char **argv)
 		len_temp = ft_strlen(temp);
 		if (len_temp > 0 && temp[len_temp - 1] == '\n')
 			temp[len_temp - 1] = '\0';
-		map[i] = ft_strdup(temp);
+		check.map[i] = ft_strdup(temp);
 		i++;
 	}
 	close(fd);
 	i = 0;
-	while (map[i])
+	while (check.map[i])
 	{
-		printf ("map[%d] = %s\n", i, map[i]);
+		printf ("map[%d] = %s\n", i, check.map[i]);
 		i++;
 	}
 	printf ("argc = %d\nargv = %s\n", argc, argv[1]);
-	if (check_size_map(size, map) == 0 || check_in_map(size, map) == 0)
-		return(printf("error in map formatting\n"), 1);
-	return (0);
+	if (check_size_map(&check) == 0 || check_in_map(&check) == 0)
+		return (printf("error in map formatting\n"), 1);
+	if (flood_fil_launch(&check) == 0)
+		return (printf("fuck floodfill\n"), 0);
+	else
+		return (0);
 }
--- 42_so_long/test.c
+++ 42_so_long/test.c
@@ -1,18 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                         ::::::::           */
+/*   test.c                                              :+:    :+:           */
+/*                                                      +:+                   */
+/*   By: mbruyere <marvin@42.fr>                       +#+                    */
+/*                                                    +#+                     */
+/*   Created: 2026/02/10 14:27:53 by mbruyere       #+#    #+#                */
+/*   Updated: 2026/02/10 14:29:57 by mbruyere       ########   odam.nl        */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "mlx.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 typedef struct s_app
 {
-    void	*mlx;
-    void	*win;
+	void	*mlx;
+	void	*win;
+	void	*img_background;
+	void	*img_wall;
 	void	*img_player;
 	int		size;
-}   t_app;
+}	t_app;
 
-int close_window(void *param)
+int	close_window(void *param)
 {
-	t_app *app;
+	t_app	*app;
 
 	app = (t_app *) param;
 	if (app->win)
@@ -25,20 +39,24 @@ int close_window(void *param)
 int	witch_key(int keycode, void *param)
 {
 	if (keycode == 65307)
-		return (close_window(param),0);
+		return (close_window(param), 0);
 }
 
-int main(void)
+int	main(void)
 {
-	t_app app;
+	t_app	app;
 
 	app.size = 128;
 	app.mlx = mlx_init();
-	app.img_player = mlx_xpm_file_to_image(app.mlx, "supra.xpm", &app.size, &app.size);
-	if (!app.img_player)
-		return(1);
+	app.img_background = mlx_xpm_file_to_image(app.mlx, "background.xpm", &app.size, &app.size);
+	app.img_wall = mlx_xpm_file_to_image(app.mlx, "wall.xpm", &app.size, &app.size);
+	app.img_player = mlx_xpm_file_to_image(app.mlx, "test.xpm", &app.size, &app.size);
+	if (!(app.img_player || app.img_background || app.img_wall))
+		return (printf("xpm not ok"), 1);
 	app.win = mlx_new_window(app.mlx, 800, 600, "Test MLX");
-	mlx_put_image_to_window(app.mlx, app.win, app.img_player, 0, 0);
+	mlx_put_image_to_window(app.mlx, app.win, app.img_background, 0, 0);
+	mlx_put_image_to_window(app.mlx, app.win, app.img_wall, 256, 0);
+	mlx_put_image_to_window(app.mlx, app.win, app.img_player, 128, 0);
 	mlx_key_hook(app.win, witch_key, &app);
 	mlx_hook(app.win, 17, 0, close_window, &app);
 	mlx_loop(app.mlx);
